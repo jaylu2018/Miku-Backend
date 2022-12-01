727da853e240959f209057b1fdf2071f1a522dd7
luyh <jaylu1995@outlook.com>
2022-12-02 00:11:17 +0800

    初始化项目

--- utils/serializers/base_serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class BaseModelSerializer(serializers.ModelSerializer):
-    create_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True, help_text='创建时间')
-    update_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True, help_text='更新时间')
